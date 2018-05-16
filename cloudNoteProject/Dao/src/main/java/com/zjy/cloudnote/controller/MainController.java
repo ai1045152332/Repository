2f30808dfc45f1d3c95aa39811bb2112873c9fcb
ai1045152332 <1045152332@qq.com>
2018-05-16 18:24:55 +0800

    22
    2

--- cloudNoteProject/Dao/src/main/java/com/zjy/cloudnote/controller/MainController.java
+++ cloudNoteProject/Dao/src/main/java/com/zjy/cloudnote/controller/MainController.java
@@ -17,29 +17,36 @@ public class MainController {
 
     @RequestMapping("/index")
     public String index(){
-        return "admin/login";
+        System.out.println("ni hao ");
+        return "index";
+    }
+    @RequestMapping("/login")
+    public String login(){
+        System.out.println("ni hao ");
+        return "login";
     }
     @RequestMapping("/initLogin")
     public String initLogin(Model model){
         model.addAttribute("model", "model:你被支持吗?");
         return "admin/img";
     }
-    @PostMapping(value="/login")
-    public String sayHello(@RequestParam("username") String loginName,
-                           @RequestParam("password") String password,
-                           @RequestParam("code") String text,
-                           HttpSession session){
-        String returnLoginName = userService.loginCheck(loginName, password);
-        if(returnLoginName!=null){
-            System.out.println(session);
-            session.setAttribute("loginName",returnLoginName);
-            session.setAttribute("id",11);
-            return "hello "+loginName+password+text + returnLoginName;
-        }else{
-            return "login over";
-        }
-
-    }
+//    @PostMapping(value="/login")
+//    @ResponseBody
+//    public String sayHello(@RequestParam("username") String loginName,
+//                           @RequestParam("password") String password,
+//                           @RequestParam("code") String text,
+//                           HttpSession session){
+//        String returnLoginName = userService.loginCheck(loginName, password);
+//        if(returnLoginName!=null){
+//            System.out.println(session);
+//            session.setAttribute("loginName",returnLoginName);
+//            session.setAttribute("id",11);
+//            return "hello "+loginName+password+text + returnLoginName;
+//        }else{
+//            return "login over";
+//        }
+//
+//    }
 
     @GetMapping("/logout")
     public String logout(HttpSession session){
---
+++ cloudNoteProject/Dao/src/main/resources/templates/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zfl
+  Date: 2018/5/16
+  Time: 18:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    login
+</body>
+</html>
