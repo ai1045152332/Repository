bb09f1acf9ddde367a8844bc8e830aee9098f35d
zjy <1045152332@qq.com>
2018-05-19 22:43:37 +0800

    mapper修改
    mapper修改

---
+++ cloudNoteProject/Dao/src/main/java/com/zjy/cloudnote/entity/CnGroup.java
@@ -0,0 +1,33 @@
+package com.zjy.cloudnote.entity;
+
+public class CnGroup {
+    private Long groupId;
+
+    private String groupName;
+
+    private Long schoolId;
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName == null ? null : groupName.trim();
+    }
+
+    public Long getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(Long schoolId) {
+        this.schoolId = schoolId;
+    }
+}
