0b0956e7cdff3d9b45845c282a71c9eb9f368619
zjy <1045152332@qq.com>
2018-06-26 23:09:28 +0800

    gradle_hellooword
    gradle_hellooword

--- MyBlog/blog_helloworld/src/main/java/com/zjy/blog/blog_start/HelloWorldApplication.java
+++ MyBlog/blog_helloworld/src/main/java/com/zjy/blog/blog_start/HelloWorldApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogStartApplication {
+public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogStartApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 }
---
+++ MyBlog/blog_helloworld/src/test/java/com/zjy/blog/blog_start/controller/ControllerApplicationTests.java
@@ -0,0 +1,29 @@
+package com.zjy.blog.blog_start.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.hamcrest.Matchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testHello() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.content().string(Matchers.equalTo("hello world")));
+	}
+
+}
