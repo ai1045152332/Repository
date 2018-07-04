d0cff8abdb0b61e6b0c02fde1d448ba45cee7373
Zhaojianyu <1045152332@qq.com>
2018-07-04 19:17:25 +0800

    文件服务器
    文件服务器

---
+++ MyBlog/blog_zjy/src/main/java/com/zjy/blog/blog_start/service/BlogServiceImpl.java
@@ -0,0 +1,63 @@
+package com.zjy.blog.blog_start.service;
+
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.zjy.blog.blog_start.domain.Blog;
+import com.zjy.blog.blog_start.domain.User;
+import com.zjy.blog.blog_start.repository.BlogRepository;
+
+/**
+ * Blog 服务.
+ */
+@Service
+public class BlogServiceImpl implements BlogService {
+
+	@Autowired
+	private BlogRepository blogRepository;
+ 
+	@Transactional
+	@Override
+	public Blog saveBlog(Blog blog) {
+		Blog returnBlog = blogRepository.save(blog);
+		return returnBlog;
+	}
+
+	@Transactional
+	@Override
+	public void removeBlog(Long id) {
+		blogRepository.delete(id);
+	}
+
+	@Override
+	public Blog getBlogById(Long id) {
+		return blogRepository.findOne(id);
+	}
+
+	@Override
+	public Page<Blog> listBlogsByTitleVote(User user, String title, Pageable pageable) {
+		// 模糊查询
+		title = "%" + title + "%";
+		String tags = title;
+		Page<Blog> blogs = blogRepository.findByTitleLikeAndUserOrTagsLikeAndUserOrderByCreateTimeDesc(title,user, tags,user, pageable);
+		return blogs;
+	}
+
+	@Override
+	public Page<Blog> listBlogsByTitleVoteAndSort(User user, String title, Pageable pageable) {
+		// 模糊查询
+		title = "%" + title + "%";
+		Page<Blog> blogs = blogRepository.findByUserAndTitleLike(user, title, pageable);
+		return blogs;
+	}
+
+	@Override
+	public void readingIncrease(Long id) {
+		Blog blog = blogRepository.findOne(id);
+		blog.setReadSize(blog.getReadSize()+1); // 在原有的阅读量基础上递增1
+		this.saveBlog(blog);
+	}
+}
--- MyBlog/mongodb-file-server-master/src/test/java/com/waylau/spring/boot/fileserver/ApplicationTests.java
+++ MyBlog/mongodb-file-server-master/src/test/java/com/waylau/spring/boot/fileserver/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zjy.blog.blog_start;
+package com.waylau.spring.boot.fileserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BlogStartApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
