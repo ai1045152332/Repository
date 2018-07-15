790e47a8e072f0ccc9f4e7d2a2fa72aed5bab378
zjy <1045152332@qq.com>
2018-07-15 11:19:07 +0800

    点赞系统前端
    点赞

--- MyBlog/blog_zjy/src/main/resources/static/js/userspace/blog.js
+++ MyBlog/blog_zjy/src/main/resources/static/js/userspace/blog.js
@@ -112,4 +112,59 @@ $(function() {
 
 	// 初始化 博客评论
 	getCommnet(blogId);
+	
+	// 提交点赞
+	$(".blog-content-container").on("click","#submitVote", function () { 
+	    // 获取 CSRF Token 
+	    var csrfToken = $("meta[name='_csrf']").attr("content");
+	    var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+
+	    $.ajax({ 
+	         url: '/votes', 
+	         type: 'POST', 
+	         data:{"blogId":blogId},
+	         beforeSend: function(request) {
+	             request.setRequestHeader(csrfHeader, csrfToken); // 添加  CSRF Token 
+	         },
+	         success: function(data){
+	             if (data.success) {
+	                 toastr.info(data.message);
+	                    // 成功后，重定向
+	                 window.location = blogUrl;
+	             } else {
+	                 toastr.error(data.message);
+	             }
+	         },
+	         error : function() {
+	             toastr.error("error!");
+	         }
+	     });
+	});
+
+	// 取消点赞
+	$(".blog-content-container").on("click","#cancelVote", function () { 
+	    // 获取 CSRF Token 
+	    var csrfToken = $("meta[name='_csrf']").attr("content");
+	    var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+
+	    $.ajax({ 
+	         url: '/votes/'+$(this).attr('voteId')+'?blogId='+blogId, 
+	         type: 'DELETE', 
+	         beforeSend: function(request) {
+	             request.setRequestHeader(csrfHeader, csrfToken); // 添加  CSRF Token 
+	         },
+	         success: function(data){
+	             if (data.success) {
+	                 toastr.info(data.message);
+	                // 成功后，重定向
+	                 window.location = blogUrl;
+	             } else {
+	                 toastr.error(data.message);
+	             }
+	         },
+	         error : function() {
+	             toastr.error("error!");
+	         }
+	     });
+	});
 });
