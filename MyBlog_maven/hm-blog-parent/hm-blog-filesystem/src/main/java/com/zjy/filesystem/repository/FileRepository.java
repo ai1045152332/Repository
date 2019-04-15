8ef4133bc4767f3f789dc87d05dbeb32f0bb17c6
zhaojy <zhaojy@ti-net.com.cn>
2019-04-15 18:42:01 +0800

    delete

--- MyBlog_maven/hm-blog-parent/hm-blog-filesystem/src/main/java/com/zjy/filesystem/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zjy.filesystem.repository;
-
-import com.zjy.filesystem.domain.UploadFile;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FileRepository extends MongoRepository<UploadFile, String> {
-}
--- MyBlog_maven/hm-blog-parent/hm-blog-web/src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO user (id, username, password, name, email) VALUES (1, 'admin', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', 'admin', '1045152332@qq.com');
-INSERT INTO user (id, username, password, name, email) VALUES (2, 'zhaojianyu', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', 'love', '1045152332@163.com');
-
-INSERT INTO authority (id, name) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO authority (id, name) VALUES (2, 'ROLE_USER');
-
-INSERT INTO user_authority (user_id, authority_id) VALUES (1, 1);
-INSERT INTO user_authority (user_id, authority_id) VALUES (2, 2);
