4bcfc0be8e705b8db5d5cf5d82bf447aae4fc469
zhaojy <zhaojy@ti-net.com.cn>
2019-10-30 23:24:22 +0800

    delete

--- BusSystem/src/com/zjy/BusFactory/BusFactory.java
+++
@@ -1,8 +0,0 @@
-package com.zjy.BusFactory;
-
-import com.zjy.entity.*;
-import com.zjy.system.BusSystem;
-
-public interface BusFactory {
-	Bus createBus(Integer id, String name ,String[] station,BusSystem bs);
-}
